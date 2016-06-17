aedc7359231909f174f5d052f7a67f7abcc5a74f
lzzdean <lzzdean@gmail.com>
2016-06-16 23:07:35 -0500

    Submission

---
+++ src/IndiProject/Data/Migrations/20160617014825_morestuff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IndiProject.Data.Migrations
+{
+    public partial class morestuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Albums",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Albums",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Albums");
+        }
+    }
+}
